f4a0f811ea7b437bc814724ca53c2b7cef45301d
kunal <patelkunal89@gmail.com>
2018-05-04 16:25:52 +0530

    awesome help message

--- java-application/src/main/java/MainApp.java
+++ java-application/src/main/java/MainApp.java
@@ -1,5 +1,13 @@
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("This is DUMMY MAIN method, use -PmainClass=package.Main to run your main class");
+        final String helpMessage = String.format("%s\n\t%s\n\t%s",
+            "usage:",
+            "gradle run -PmainClass=package.Main",
+            "gradle run -PmainClass=package.Main -Dexec.args=\"arg1 arg2 arg3 1 2 3\""
+        );
+        System.out.println(helpMessage);
     }
 }
